cb4d68ab04ef7b9f836f6f121228d497c6b6130b
Vasiliy Voronko <mail@voronko.dev>
2023-08-27 10:24:12 +0300

    Добавил шаблон блейзор проекта соедененного с WebApi

--- Finik/Finik.Web/Program.cs
+++ Finik/Finik.Web/Program.cs
@@ -5,15 +5,21 @@ using Finik.DbData;
 using Finik.Infrastructure.Mappers;
 using Finik.Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Finik", Version = "v1" });
+});
 
 builder.Services.AddScoped<INewsManager, NewsManager>();
 builder.Services.AddScoped<INewsDbRepository, NewsEfRepository>();
@@ -28,16 +34,29 @@ builder.Services.AddDbContext<FinikDbContext>(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+else
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    });
 }
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.UseStaticFiles();
+
+app.UseRouting();
 
 app.MapControllers();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
 
 app.Run();
